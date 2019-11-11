89c7f5bb9e502d363438e9a1a02af447c8579733
ilyasyasin072 <ilyas@mewahniagajaya.com>
2019-11-11 10:06:02 +0700

    memuat fullname dengan mutator

--- app/User.php
+++ app/User.php
@@ -6,8 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
+    protected $appends = ['fullname'];
+    
     public function setPasswordAttribute($password)
     {
         $this->attributes['password'] = \Hash::make($password);
+    }
+
+    public function getFullnameAttribute()
+    {
+        return $this->firstname . ' ' . $this->lastname;
     }
 }
