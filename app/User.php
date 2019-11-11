86fc5a2c3c9723b7eb95c3deef5a1aa52ab4980d
ilyasyasin072 <ilyas@mewahniagajaya.com>
2019-11-11 10:15:31 +0700

    assesor mutator

--- app/User.php
+++ app/User.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
     protected $appends = ['fullname'];
-    
+
     public function setPasswordAttribute($password)
     {
         $this->attributes['password'] = \Hash::make($password);
@@ -16,5 +16,16 @@ class User extends Model
     public function getFullnameAttribute()
     {
         return $this->firstname . ' ' . $this->lastname;
+    }
+
+    public function setBirthDateAttribute($date)
+    {
+        $this->attributs['birth_date'] = 
+        \Carbon\Carbon::createFormFormat('d/m/y', $date)->toDateString();
+    }
+
+    public function getBithDateAttribute($date)
+    {
+        return \Carbon\Carbon::createFromFormat('Y-m-d', $date)->format('d/m/y');
     }
 }
